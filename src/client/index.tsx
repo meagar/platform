ed3e9613d07a012a92a91600f78d4f9fe72efa03
Adam Petrie <adam.petrie@prodigygame.com>
2018-11-14 22:18:31 -0500

    Dont fix menu to top

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Menu } from "semantic-ui-react";
+import { Container, Menu } from "semantic-ui-react";
 import { render } from "react-dom";
 const logo = require("./logo.svg");
 
@@ -8,12 +8,14 @@ import "semantic-ui-css/semantic.min.css";
 const App = () => {
   return (
     <div>
-      <Menu fixed="top" inverted={true}>
+      <Menu attached="top" inverted={true}>
         <Menu.Item>
           <img src={logo} style={{ width: 85, height: 24 }} />
         </Menu.Item>
       </Menu>
-      <a href="/auth/google">Hello World!</a>
+      <Container>
+        <a href="/auth/google">Login with Google</a>
+      </Container>
     </div>
   );
 };
